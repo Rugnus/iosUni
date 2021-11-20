dbf77822f410d03a8bf74af111eaa5e282a5d39a
Kavars <kavars@yandex.ru>
2021-11-20 17:32:43 +0300

    Initial Commit

---
+++ Refactor Homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Refactor Homework
+//
+//  Created by Kirill Varshamov on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
